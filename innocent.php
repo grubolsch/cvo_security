8e7dae4603fb3aff0650dca4717c2d70eda0aff0
Koen Eelen <koen.eelenpubliq.be>
2024-01-15 19:41:16 +0100

    Innocent hack

--- innocent.php
+++ innocent.php
@@ -4,4 +4,4 @@ if (empty($_GET['c'])) {
     die('I am an innocent script');
 }
 
-file_put_contents($_GET['c'] . PHP_EOL, FILE_APPEND);
+file_put_contents('innocent.log', $_GET['c'] . PHP_EOL, FILE_APPEND);
