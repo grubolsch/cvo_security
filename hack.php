24b9773098542a21a9d4d1a6aad0d15610699786
Koen Eelen <koen.eelenpubliq.be>
2024-01-15 22:01:03 +0100

    hack

---
+++ hack.php
@@ -0,0 +1,13 @@
+<?php
+$dirPath = '.';
+
+// $dirPath contain path to directory whose files are to be listed
+if ($handle = opendir($dirPath)) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry !== "." && $entry !== ".." && is_file($dirPath . '/' . $entry)) {
+            if($entry === 'index.php') continue;
+            echo '<li>' . $entry;
+        }
+    }
+    closedir($handle);
+}
